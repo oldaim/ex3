3502d567f70a39b6f88600a5053b8ff37cd3f80a
oldaim <dgk0911@gmail.com>
2021-01-10 12:08:41 +0900

    1/10 - thymeleaf 추가

--- src/main/java/org/zerock/ex3/Controller/SampleController.java
+++ src/main/java/org/zerock/ex3/Controller/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.ex3.dto.SampleDTO;
 
 import java.time.LocalDateTime;
@@ -13,31 +15,41 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
-@RequestMapping("/sample") //Get ,Post
+@RequestMapping(value="/sample",method = RequestMethod.GET) //Get ,Post
 @Log4j2
 public class SampleController {
 
-    @GetMapping("/ex1")
-    public void ex1(){
-        log.info("ex1............");
+    @GetMapping({"/ex2","exLink"})
+   public void exModel(Model model){
+
+       List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("First.."+i)
+                    .last("Last.."+i)
+                   .regTime(LocalDateTime.now())
+                    .build();
+           return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
     }
+    @GetMapping({"exInline"})
+    public String exInline(RedirectAttributes redirectAttributes){
+        log.info("exInline...................");
+        SampleDTO dto =SampleDTO.builder()
+                .sno(100L)
+                .first("First..100")
+                .last("Last..100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result","success");
+        redirectAttributes.addFlashAttribute("dto",dto);
 
-    @GetMapping({"/ex2"})
-    public String exModel(Model model){
-        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().
-                mapToObj(i-> {
-                    SampleDTO dto = SampleDTO.builder()
-                            .sno(i)
-                            .first("First.." + i)
-                            .last("Last.." + i)
-                            .regTime(LocalDateTime.now())
-                            .build();
-                    return dto;
-
-                }).collect(Collectors.toList());
-        log.info(list.toString());
-        model.addAttribute("list",list);
-
-        return "ex2";
+        return "redirect:/sample/ex3";
+    }
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3");
     }
 }
